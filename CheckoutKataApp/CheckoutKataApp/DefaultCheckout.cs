6462351d9222e3791045758d181a9a41b8860630
JonBurtyn <jonburto@gmail.com>
2016-02-28 18:51:30 +0000

    Add Test project. Setup ICheckout.

---
+++ CheckoutKataApp/CheckoutKataApp/DefaultCheckout.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CheckoutKataApp
+{
+    public class DefaultCheckout : ICheckout
+    {
+        public void Scan(string item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetTotalPrice()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CheckoutKataApp/CheckoutKataApp/ICheckout.cs
@@ -0,0 +1,8 @@
+﻿namespace CheckoutKataApp
+{
+    public interface ICheckout
+    {
+        void Scan(string item);
+        int GetTotalPrice();
+    }
+}
