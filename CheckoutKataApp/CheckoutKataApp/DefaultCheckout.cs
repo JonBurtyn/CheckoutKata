5c0a58509a16336e2421e6b263f0d47499553e42
JonBurtyn <jonburto@gmail.com>
2016-02-28 19:03:11 +0000

    Making second test pass.

--- CheckoutKataApp/CheckoutKataApp/DefaultCheckout.cs
+++ CheckoutKataApp/CheckoutKataApp/DefaultCheckout.cs
@@ -1,17 +1,24 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace CheckoutKataApp
 {
     public class DefaultCheckout : ICheckout
     {
+        private List<string> items = new List<string>();
+
         public void Scan(string item)
         {
-            
+            this.items.Add(item);
         }
 
         public int GetTotalPrice()
         {
-            return 50;
+            if (items.Any())
+                return 50;
+
+            return 0;
         }
     }
 }
