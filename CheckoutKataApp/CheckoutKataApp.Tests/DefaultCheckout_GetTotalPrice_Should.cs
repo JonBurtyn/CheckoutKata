6e238659c887c0cf64822d7633d54fbba21d77bb
JonBurtyn <jonburto@gmail.com>
2016-02-28 21:59:18 +0000

    Adding test for combined unit and special prices.

--- CheckoutKataApp/CheckoutKataApp.Tests/DefaultCheckout_GetTotalPrice_Should.cs
+++ CheckoutKataApp/CheckoutKataApp.Tests/DefaultCheckout_GetTotalPrice_Should.cs
@@ -86,5 +86,22 @@ namespace CheckoutKataApp.Tests
 
             Assert.Equal(130, total);
         }
+
+        [Fact]
+        public void ReturnCombinedUnitAndSpecialPriceOf180ForFourAs()
+        {
+            var configuredPrices = new Dictionary<string, Prices>();
+            configuredPrices["A"] = new Prices() { UnitPrice = 50, SpecialPrice = new SpecialPrice() { Quantity = 3, Price = 130 } };
+            var checkout = new DefaultCheckout(configuredPrices);
+
+            checkout.Scan("A");
+            checkout.Scan("A");
+            checkout.Scan("A");
+            checkout.Scan("A");
+
+            var total = checkout.GetTotalPrice();
+
+            Assert.Equal(180, total);
+        }
     }
 }
